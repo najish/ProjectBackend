58890ab797df9c24058c0ee31a3850e1be05dca5
anonymous <anonymos@gmail.com>
2024-03-30 22:38:48 +0530

    form validation process added but not completed

--- src/Components/Forms/SignUp.jsx
+++ src/Components/Forms/SignUp.jsx
@@ -1,31 +1,59 @@
+import { useState } from 'react'
 import './SignUp.css'
 
 function SignUp() {
 
+    const [formData, setFormData] = useState({
+        username: '',
+        name: '',
+        email:'',
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    
+    const changeHandler = (e) => {
+        console.log('change handller')
+        const {name, value} = e.target
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(formData)
+
+        const {name, username, email, password} = formData;
+        
+
+    }
 
     return (
         <>
             <h1 className="signup">SignUp</h1>
-            <form method='post' action='#' id='signup'>
+            <form method='post' id='signup' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input type='text' placeholder='enter name..' name='name' id='name' />
+                    <input type='text' placeholder='enter name..' name='name' value={formData.name}  id='name' onChange={changeHandler}/>
                 </div>
 
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type='email' placeholder='email..' name='email' id='email' />
+                    <input type='email' placeholder='email..' name='email' value={formData.email}  id='email' onChange={changeHandler} />
                 </div>
 
                 <div>
                     <label htmlFor="username">Username</label>
-                    <input type='text' placeholder='username..' name='username' id='username' />
+                    <input type='text' placeholder='username..' name='username' value={formData.username}  id='username' onChange={changeHandler} />
                 </div>
 
 
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type='text' placeholder='password..' name='password' id='password' />
+                    <input type='text' placeholder='password..' name='password' value={formData.password} id='password' onChange={changeHandler} />
                 </div>
                 <div>
                     <input type="submit" value='SignUp' />
